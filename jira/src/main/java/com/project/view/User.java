c0cd38af26b3e0851f0c4f6c983fe9bce1445e1e
nemo-Capt <vlad060600@gmail.com>
2020-12-22 13:49:14 +0300

    qwerty

--- jira/src/main/java/com/project/view/User.java
+++ jira/src/main/java/com/project/view/User.java
@@ -1,10 +1,17 @@
 package com.project.view;
-import javax.persistence.Entity;
-import javax.persistence.Id;
+
+import javax.persistence.*;
 
 @Entity
 public class User {
 
-
-
+    @Id
+    private long id;
+    private String username;
+    private String email;
+    private String password;
+    private boolean banned;
+    @ManyToOne
+    @JoinColumn(name="cart_id", nullable=false)
+    private Role role;
 }
